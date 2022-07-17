1bba9fec6fa66d49bd93c5fbcba42154de9e905b
Jalome <chjalome@gmail.com>
2022-07-17 14:47:49 +0200

    initial upload

--- App.js
+++ App.js
@@ -1,12 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { LoginManager, AccessToken } from 'react-native-fbsdk-next';
+import app from './firebasSetup';
+import {
+  getAuth,
+  signInWithCredential,
+  FacebookAuthProvider,
+} from 'firebase/auth';
 
 export default function App() {
+
+  const signInWithFB = async () => {
+    // Attempt login with permissions
+    const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
+    if (result.isCancelled) {
+      throw 'User cancelled the login process';
+    }
+    // Once signed in, get the users AccesToken
+    const data = await AccessToken.getCurrentAccessToken();
+    if (!data) {
+      throw 'Something went wrong obtaining access token';
+    }
+    
+    const auth = getAuth(app);
+
+    // Create a Firebase credential with the AccessToken
+    const facebookAuthProvider = FacebookAuthProvider.credential(data.accessToken);
+    // console.log("provider ",facebookAuthProvider);
+    // const credential = facebookAuthProvider.credential(data.accessToken);
+    // Sign-in with credential from the Facebook user.
+    signInWithCredential(auth, facebookAuthProvider)
+    .then(() => {
+
+    })
+    .catch(error => {
+      // Handle Errors here.]
+      console.log(error);
+    });
+
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Button title="Sign In With FB" onPress={signInWithFB}/>
     </View>
   );
 }
---
+++ firebasSetup.js
@@ -0,0 +1,9 @@
+import { initializeApp } from 'firebase/app';
+
+// Initialize Firebase
+const firebaseConfig = {
+
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
